9e8828fb5bb9845b12caa833e90609973871343c
Jonatas <jonatas.coelho@aluno.faculdadeimpacta.com.br>
2020-07-16 23:44:09 -0300

    Classe FormaPagamentoController concluida

--- src/main/java/br/com/coelho/pedidos/controller/FormaPagamentoController.java
+++ src/main/java/br/com/coelho/pedidos/controller/FormaPagamentoController.java
@@ -8,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.coelho.pedidos.repository.enties.FormaPagamentoEntity;
@@ -38,7 +43,69 @@ public class FormaPagamentoController {
 	}
 
 	@GetMapping("/{codigo}")
-	public ResponseEntity<?> getByCodigo(@PathVariable(name = "codigo") Long codigo) {
+	public ResponseEntity<?> getById(@PathVariable(name = "id") Long id) {
+		try {
+			logger.info("Acessando funcionalidade de busca de forma de pagamento por id");
+			Optional<FormaPagamentoEntity> formaPgtoOpt = formaPagamento.findById(id);
+			if (!formaPgtoOpt.isPresent()) {
+				logger.info("Forma de pagamento com id %d não cadastrada", id);
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<>(formaPgtoOpt.get(), HttpStatus.OK);
+		} catch (Exception e) {
+			logger.error("Erro em buscar forma de pagamento com id %d erro: %s", id, e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<?> cadastrarFormaPagamento(@RequestBody FormaPagamentoEntity formaPagamentoEntity) {
+		try {
+			logger.info("Acessando funcionalidade de cadastro de forma de pagamento");
+			return new ResponseEntity<>(formaPagamento.save(formaPagamentoEntity), HttpStatus.CREATED);
+		} catch (Exception e) {
+			logger.error("Erro em cadastrar forma de pagamento %s" + formaPagamentoEntity.getDescricaoFormaDePgto());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> alterarFormaPagamento(@PathVariable(name = "id") Long id,
+			@RequestBody FormaPagamentoEntity formaPgtoEntity) {
+		try {
+			logger.info("Acessando funcionalidade de alterar forma de pagamento");
+			Optional<FormaPagamentoEntity> formaPgtoOpt = formaPagamento.findById(id);
+			if (!formaPgtoOpt.isPresent()) {
+				logger.info("Forma de pagamento com id %d não cadastrada", id);
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			formaPgtoEntity.setIdFormaDePgto(id);
+			return new ResponseEntity<>(formaPagamento.save(formaPgtoEntity), HttpStatus.OK);
+		} catch (Exception e) {
+			logger.error("Erro em alterar forma de pagamento com id %d erro: %s", id, e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletarFormaPagamento(@PathVariable(name = "id") Long id) {
+		try {
+			logger.info("Acessando funcionalidade de deletar forma de pagamento");
+			Optional<FormaPagamentoEntity> formaPgtoOpt = formaPagamento.findById(id);
+			if (!formaPgtoOpt.isPresent()) {
+				logger.info("Forma de pagamento com id %d não cadastrada", id);
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			formaPagamento.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			logger.error("Erro em deletar forma de pagamento com id %d erro: %s", id, e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping
+	public ResponseEntity<?> getByCodigo(@RequestParam(name = "codigo") Long codigo) {
 		try {
 			logger.info("Acessando funcionalidade de busca de forma de pagamento por codigo");
 			Optional<FormaPagamentoEntity> formaPgtoOpt = formaPagamento.findByCodigoFormaDePgto(codigo);
